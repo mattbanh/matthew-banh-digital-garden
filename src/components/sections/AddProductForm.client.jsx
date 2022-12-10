cce46c1fa69fccdb628aa84a5a0d04a1c7b9ea53
Matthew Banh <mattbanh92@gmail.com>
2022-12-09 18:06:25 -0800

    tested graphql queries and separated add product form in to a client component

---
+++ src/components/sections/AddProductForm.client.jsx
@@ -0,0 +1,159 @@
+export function AddProductForm({data}) {
+  return (
+    <>
+      <div>{data ? <h1>hi</h1> : <h1>no</h1>}</div>
+      <div>
+        <h1>{data}</h1>
+      </div>
+      <form className="w-full">
+        <div className="flex flex-wrap -mx-3 mb-6">
+          <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+            <label
+              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              htmlFor="grid-first-name"
+            >
+              Product Name
+            </label>
+            <input
+              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+              id="grid-first-name"
+              type="text"
+              placeholder="Jane"
+            />
+            {/* <p className="text-red-500 text-xs italic">
+              Please fill out this field.
+            </p> */}
+          </div>
+          <div className="w-full md:w-1/2 px-3">
+            <label
+              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              htmlFor="grid-last-name"
+            >
+              Last Name
+            </label>
+            <input
+              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              id="grid-last-name"
+              type="text"
+              placeholder="Doe"
+            />
+          </div>
+        </div>
+        <div className="flex flex-wrap -mx-3 mb-6">
+          <div className="w-full px-3">
+            <label
+              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              htmlFor="grid-password"
+            >
+              Description
+            </label>
+            <input
+              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              id="grid-password"
+              type="textarea"
+              rows="4"
+              placeholder=""
+            />
+          </div>
+        </div>
+        <div className="flex flex-wrap -mx-3 mb-6">
+          <div className="w-full px-3">
+            <label
+              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              htmlFor="grid-password"
+            >
+              Product Image
+            </label>
+            <input
+              // className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              id="grid-password"
+              type="file"
+              accept="image/png, image/jpeg, image/jpg"
+              rows="4"
+              placeholder=""
+            />
+          </div>
+        </div>
+        <div className="flex flex-wrap -mx-3 mb-2">
+          <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+            <label
+              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              htmlFor="grid-city"
+            >
+              City
+            </label>
+            <input
+              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              id="grid-city"
+              type="text"
+              placeholder="Albuquerque"
+            />
+          </div>
+          <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+            <label
+              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              htmlFor="grid-state"
+            >
+              State
+            </label>
+            <div className="relative">
+              <select
+                className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                id="grid-state"
+              >
+                <option>New Mexico</option>
+                <option>Missouri</option>
+                <option>Texas</option>
+              </select>
+            </div>
+          </div>
+          <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+            <label
+              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              htmlFor="grid-zip"
+            >
+              Zip
+            </label>
+            <input
+              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              id="grid-zip"
+              type="text"
+              placeholder="90210"
+            />
+          </div>
+        </div>
+      </form>
+      {/* <form className="flex-col">
+        <div className="flex">
+          <label>
+            Vendor
+            <input
+              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+              type="text"
+              defaultValue={data.customerAccessToken}
+              placeholder="Product Name"
+            ></input>
+          </label>
+          <label>
+            Customer Name
+            <input
+              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+              type="text"
+              defaultValue={data.customer.firstName}
+              placeholder="Product Name"
+            ></input>
+          </label>
+          <label>
+            Status
+            <input
+              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+              type="text"
+              defaultValue="Active"
+              placeholder="Product Name"
+            ></input>
+          </label>
+        </div>
+      </form> */}
+    </>
+  );
+}
--- src/routes/add-product/index.server.jsx
+++ src/routes/add-product/index.server.jsx
@@ -17,10 +17,12 @@ import {
   useLocalization,
   gql,
 } from '@shopify/hydrogen';
+
 import axios from 'axios';
 
 import {Layout} from '~/components/index.server';
 import {Section} from '../../components/index';
+import {AddProductForm} from '../../components/sections/AddProductForm.client';
 
 export default function AddProduct() {
   // if (!customerAccessToken) return response.redirect('/account/login');
@@ -29,54 +31,20 @@ export default function AddProduct() {
     <Layout>
       <Section>
         <h1 className="font-bold text-4xl">Add Product</h1>
-        <AddProductForm />
-        <TestQuery />
+        <AddProductFormComp />
+        {/* <TestQuery /> */}
       </Section>
     </Layout>
   );
 }
 
-function postThis() {
-  axios.post(
-    'https://hydrogenappyo.myshopify.com/admin/api/2022-10/products.json',
-    {
-      product: {
-        title: 'Toy2',
-        body_html: 'The best best product',
-        vendor: 'Matt',
-      },
-    },
-    {
-      headers: {
-        'X-Shopify-Access-Token': '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',
-      },
-    },
-  );
-}
-
-function TestQuery() {
-  const {data} = useShopQuery({
-    query: PRODUCTS,
-  });
-  postThis();
-  return (
-    <div>
-      <h1>hi</h1>
-      {data.products.nodes.map((product) => (
-        <h1 key={product.id}>{product.id}</h1>
-      ))}
-    </div>
-  );
-}
-
-function AddProductForm() {
+function AddProductFormComp() {
   const {customerAccessToken} = useSession();
 
   const {
     language: {isoCode: languageCode},
     country: {isoCode: countryCode},
   } = useLocalization();
-
   const {data} = useShopQuery({
     query: CUSTOMER_QUERY,
     variables: {
@@ -87,163 +55,41 @@ function AddProductForm() {
     cache: CacheNone(),
   });
 
-  const {customer} = data;
-
-  return (
-    <>
-      <div>{data ? <h1>hi</h1> : <h1>no</h1>}</div>
-      <form className="w-full">
-        <div className="flex flex-wrap -mx-3 mb-6">
-          <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-            <label
-              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-              htmlFor="grid-first-name"
-            >
-              Product Name
-            </label>
-            <input
-              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-              id="grid-first-name"
-              type="text"
-              placeholder="Jane"
-            />
-            {/* <p className="text-red-500 text-xs italic">
-              Please fill out this field.
-            </p> */}
-          </div>
-          <div className="w-full md:w-1/2 px-3">
-            <label
-              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-              htmlFor="grid-last-name"
-            >
-              Last Name
-            </label>
-            <input
-              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-              id="grid-last-name"
-              type="text"
-              placeholder="Doe"
-            />
-          </div>
-        </div>
-        <div className="flex flex-wrap -mx-3 mb-6">
-          <div className="w-full px-3">
-            <label
-              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-              htmlFor="grid-password"
-            >
-              Description
-            </label>
-            <input
-              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-              id="grid-password"
-              type="textarea"
-              rows="4"
-              placeholder=""
-            />
-          </div>
-        </div>
-        <div className="flex flex-wrap -mx-3 mb-6">
-          <div className="w-full px-3">
-            <label
-              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-              htmlFor="grid-password"
-            >
-              Product Image
-            </label>
-            <input
-              // className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-              id="grid-password"
-              type="file"
-              accept="image/png, image/jpeg, image/jpg"
-              rows="4"
-              placeholder=""
-            />
-          </div>
-        </div>
-        <div className="flex flex-wrap -mx-3 mb-2">
-          <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-            <label
-              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-              htmlFor="grid-city"
-            >
-              City
-            </label>
-            <input
-              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-              id="grid-city"
-              type="text"
-              placeholder="Albuquerque"
-            />
-          </div>
-          <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-            <label
-              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-              htmlFor="grid-state"
-            >
-              State
-            </label>
-            <div className="relative">
-              <select
-                className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                id="grid-state"
-              >
-                <option>New Mexico</option>
-                <option>Missouri</option>
-                <option>Texas</option>
-              </select>
-            </div>
-          </div>
-          <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-            <label
-              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-              htmlFor="grid-zip"
-            >
-              Zip
-            </label>
-            <input
-              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-              id="grid-zip"
-              type="text"
-              placeholder="90210"
-            />
-          </div>
-        </div>
-      </form>
-      <form className="flex-col">
-        <div className="flex">
-          <label>
-            Vendor
-            <input
-              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-              type="text"
-              defaultValue={customerAccessToken}
-              placeholder="Product Name"
-            ></input>
-          </label>
-          <label>
-            Customer Name
-            <input
-              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-              type="text"
-              defaultValue={customer.firstName}
-              placeholder="Product Name"
-            ></input>
-          </label>
-          <label>
-            Status
-            <input
-              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-              type="text"
-              defaultValue="Active"
-              placeholder="Product Name"
-            ></input>
-          </label>
-        </div>
-      </form>
-    </>
-  );
+  return <AddProductForm data={customerAccessToken} />;
 }
+
+// function postThis() {
+//   axios.post(
+//     'https://hydrogenappyo.myshopify.com/admin/api/2022-10/products.json',
+//     {
+//       product: {
+//         title: 'Toy2',
+//         body_html: 'The best best product',
+//         vendor: 'Matt',
+//       },
+//     },
+//     {
+//       headers: {
+//         'X-Shopify-Access-Token': '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',
+//       },
+//     },
+//   );
+// }
+
+// function TestQuery() {
+//   const {data} = useShopQuery({
+//     query: PRODUCTS,
+//   });
+//   postThis();
+//   return (
+//     <div>
+//       <h1>hi</h1>
+//       {data.products.nodes.map((product) => (
+//         <h1 key={product.id}>{product.id}</h1>
+//       ))}
+//     </div>
+//   );
+// }
 
 const CUSTOMER_QUERY = gql`
   query CustomerDetails(
