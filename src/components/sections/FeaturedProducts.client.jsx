f4a3986e38b74cbc05fa19e5e95a6369c0db0b34
Matthew Banh <mattbanh92@gmail.com>
2022-12-12 17:49:22 -0800

    added button animation for featured products

--- src/components/sections/FeaturedProducts.client.jsx
+++ src/components/sections/FeaturedProducts.client.jsx
@@ -13,14 +13,15 @@ export function FeaturedProducts({products}) {
             <FeaturedProductCard product={product} key={product.id} />
           ))}
         </div>
-        <div className="flex justify-center min-w-[18%] items-center">
+        <div className="flex justify-end min-w-[18%] items-center">
           <Link
-            className="overflow-hidden rounded-full h-24 w-24 bg-pink-400  mb-24 lg:h-32 lg:w-32 "
+            className="relative group overflow-hidden rounded-full box-content border-2 border-blue-300 h-24 w-24 border-1  mb-24 lg:h-32 lg:w-32 "
             to="/products"
           >
-            <div className="bg-blue-700 w-full h-full flex justify-center items-center ">
-              <span className="block text-xs">VIEW ALL</span>
-            </div>
+            <div className="bg-blue-300 rounded-full w-full h-full flex justify-center items-center transition ease-in-out -translate-x-full group-hover:translate-x-0 duration-500"></div>
+            <span className="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 text-xs font-bold text-blue-300 group-hover:text-white transition ease-in-out duration-500">
+              VIEW ALL
+            </span>
           </Link>
         </div>
       </div>
